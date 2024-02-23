e6ad0b9bc76d7c089f063211485057ad285a5714
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-23 22:29:25 +0100

    Initial commit

---
+++ LifeGoesOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeGoesOnApp.swift
+//  LifeGoesOn
+//
+//  Created by Nicola Kaleta on 23/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct LifeGoesOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
